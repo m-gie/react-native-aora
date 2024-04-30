4f51c99f9bb7197300bf635d62cb6e22f5e2ecc9
m.glowacki <m.glowacki@smsnet.pl>
2024-04-30 15:01:37 +0200

    Add backend api

--- app/index.jsx
+++ app/index.jsx
@@ -5,8 +5,13 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
 import CustomButton from "../components/CustomButton";
 import { router, Redirect } from "expo-router";
+import { useGlobalContext } from "../context/GlobalProvider";
 
 export default function App() {
+  const { isLoading, isLoggedIn } = useGlobalContext();
+
+  if (!isLoading && isLoggedIn) return <Redirect href="/home" />;
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
