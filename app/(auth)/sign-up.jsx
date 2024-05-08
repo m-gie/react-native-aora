f24db8c5efad7f9e4dcf73e501bffb2150d78a10
m.glowacki <m.glowacki@smsnet.pl>
2024-05-08 14:00:55 +0200

    Add profile screen

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -8,6 +8,8 @@ import { Link, router } from "expo-router";
 import { createUser } from "../../lib/appwrite";
 
 const SignUp = () => {
+  const { setUser, setIsLoggedIn } = useGlobalContext();
+
   const [form, setForm] = useState({
     username: "",
     email: "",
@@ -23,7 +25,8 @@ const SignUp = () => {
     setIsSubmitting(true);
     try {
       const result = await createUser(form.email, form.password, form.username);
-      // TODO: set it to global state
+      setUser(result);
+      setIsLoggedIn(true);
       router.replace("/home");
     } catch (error) {
       Alert.alert("Error", error.message);
